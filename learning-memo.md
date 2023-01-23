26b8a024cf278d46730147279f7988fa5ff79751
hikamayogoat <hikamayo.dw@gmail.com>
2023-01-23 23:00:19 +0900

    src/App.tsxで色々やるやつ

--- learning-memo.md
+++ learning-memo.md
@@ -1,3 +1,7 @@
-# 参考にしたページ
-- [React入門（TypeScript版）](https://www.webopixel.net/javascript/1588.html)
+[React入門（TypeScript版）](https://www.webopixel.net/javascript/1588.html)
+
+- `npx create-react-app react-lesson --template typescript` じゃないと TypeScript で作られなかった
+  - 記事だと `--typescript`
+- デフォルトエクスポートについては考えることがありそう
+  - https://engineering.linecorp.com/ja/blog/you-dont-need-default-export/
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { PROPERTY_TYPES } from '@babel/types';
 
-function App() {
+type Item = {
+  id: number,
+  title: string,
+};
+
+const items: Item[] = [
+  {
+    id: 1,
+    title: "商品1"
+  },
+  {
+    id: 2,
+    title: "商品2"
+  }
+];
+
+type Props = {
+  number: string
+  children: React.ReactNode
+}
+
+const Child: React.FC<Props> = ({ number, children })=> {
+  return (
+    <p>{ number } の { children }</p>
+  )
+}
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Child number="1">子のコンポーネント1</Child>
+      <Child number="2">子のコンポーネント2</Child>
     </div>
   );
 }
